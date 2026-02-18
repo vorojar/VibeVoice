e3ae508c647ff999a7df73abc6f3b19168dbf172
vorojar <vorojar@gmail.com>
2026-02-18 16:34:09 +0800

    8000端口改为8001

--- api_server.py
+++ api_server.py
@@ -1431,4 +1431,4 @@ async def preview_saved_voice(voice_id: str):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8001)
